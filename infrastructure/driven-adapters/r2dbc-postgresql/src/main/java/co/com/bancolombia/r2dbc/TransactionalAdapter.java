d3aded842d2e97f7a6e92f8781e38339ae23b4d3
Andres Darío Peña Asprilla <andres.pena2@udea.edu.co>
2025-08-27 19:24:44 -0500

    Update TransactionalAdapter.java

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/bancolombia/r2dbc/TransactionalAdapter.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/bancolombia/r2dbc/TransactionalAdapter.java
@@ -1,18 +1,16 @@
 package co.com.bancolombia.r2dbc;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import co.com.bancolombia.model.gateways.TransactionalGateway;
 import org.springframework.transaction.reactive.TransactionalOperator;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
-
+@AllArgsConstructor
 @Slf4j
 @Component
 public class TransactionalAdapter implements TransactionalGateway {
     private final TransactionalOperator operator;
 
-    public TransactionalAdapter(TransactionalOperator operator) {
-        this.operator = operator;
-    }
 
     @Override
     public <T> Mono<T> transactional(Mono<T> action) {
